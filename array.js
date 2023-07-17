4e25ee2a907487df2ddb7740a58a60e552580203
User <untilova2001@gmail.com>
2023-07-17 23:23:10 +0300

    homework (WITH SWITCH AND IF)

--- array.js
+++ array.js
@@ -83,17 +83,18 @@ console.log(users[0].password, users[1].password, users[2].password, users[3].pa
 // 2. Логічні розгалуження:
 //    1) Є змінна х, якій ви надаєте довільне числове значення. Якщо змінна x не дорівнює нулю, виведіть 'Вірно', інакше виведіть 'Невірно'. Перевірте  скрипт
 //    при a, що дорівнює 1, 0, -3
-let x = 4;
-switch (x) {
-    case x !==0:
-        console.log("Вірно");
-        break;
-    case 0:
-        console.log("Невірно");
-        break;
-    default:
-        console.log("???")
-}
+
+    // let x = 4;
+    // switch (x) {
+    //     case x !==0:
+    //         console.log("Вірно");
+    //         break;
+    //     case 0:
+    //         console.log("Невірно");           IT'S NON-WORKING PART, REMEMBER: "CASE" ACCEPT ONLY WHAT WILL RESULT
+    //         break;
+    //     default:
+    //         console.log("???")
+    // }
 
 let x1 = 1
 if (x1 !==0) {
@@ -117,32 +118,108 @@ if (x3 !==0) {
 //    2) Дано змінну time яка рівна числу від 0 до 59. Потрібно написати код, який перевірить, до якої четверті години попадає число (в першу, другу, третю или
 //    четверту частину години).
 let time = 15
-switch (time) {
-    case time>0 && time<=15:
-        console.log("I чверть")
-        break
-    case time>15 && time<=30:
+if (time>0 && time<=15) {
+    console.log("I чверть")
+} else if (time>15 && time<=30) {
         console.log("II чверть")
+} else if (time>30 && time<=45) {
+    console.log("III чверть")
+} else if (time>45 && time<=60) {
+    console.log("IV чверть")
+} else {
+    console.log("???")
+}
+
+//    3) У змінній day дано якесь число від 1 до 31. Потрібно визначити, у яку половину(декаду) місяця потрапляє це число (у першу, другу чи третю).
+//                 SIMILAR TO THE PREVIOUS EXERCISE
+//
+//    4) Скласти розклад на тиждень за домопоги switch. Користувач вводить порядковий номер дня тижня і на екрані відображається інфа що заплановано на цей
+//    день (можна замість плану на день, назву дня англійською).
+
+let schedule = prompt("Enter day number")
+switch (schedule) {
+    case "1":
+        console.log("Monday")
         break
-    case time>30 && time<=45:
-        console.log("III чверть")
+    case "2":
+        console.log("Tuesday")
         break
-    case time>45 && time<=60:
-        console.log("IV чверть")
+    case "3":
+        console.log("Wednesday")
+        break
+    case "4":
+        console.log("Thursday")
+        break
+    case "5":
+        console.log("Friday")
+        break
+    case "6":
+        console.log("Saturday")
+        break
+    case "7":
+        console.log("Sunday")
         break
     default:
         console.log("???")
 }
 
-
-//    3) У змінній day дано якесь число від 1 до 31. Потрібно визначити, у яку половину(декаду) місяця потрапляє це число (у першу, другу чи третю).
-//    4) Скласти розклад на тиждень за домопоги switch. Користувач вводить порядковий номер дня тижня і на екрані відображається інфа що заплановано на цей
-//    день (можна замість плану на день, назву дня англійською).
 //    5) Користувач вводить або має два числа. Потрібно знайти та вивести максимальне число з тих двох. Також потрібно врахувати коли введені рівні числа.
-//
+let a=1
+let b=2
+if (a>b) {
+    console.log(a)
+} else if (b>a) {
+    console.log(b)
+} else if (a===b) {
+    console.log(a=b)
+} else {
+    console.log("???")
+}
+
 //3. Є змінна х, яка може прийняти будь-яке значення (стрінг, число, undefined, null  і тд включно). Напишіть код який, за допомоги  оператора || буде
 //    присвоювати змінній х значення "default"  якщо значення змінної х являється falsy (хибноподібні, тобто приводиться до false)
-//
-//
+let k
+if (k === "" || k === undefined || k === null || k ===0) {        /*   IDK, MAYBE RIGHT. IF U KNOW ANOTHER SOLVE, TELL ME, PLEASE :З   */
+    console.log("default")
+}
+
 //4. З файлу arrays.js (лежить в папці 2023 plan ) взяти масив coursesAndDurationArray. За допомоги іф перевірити кожен його елемент на тривалість навчання.
 //    У випадку якщо тривалість довша за 5 місяців вивести в консоль "Супер"./
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+]
+if (coursesAndDurationArray[0].monthDuration>5) {
+    console.log("Супер")
+} else {
+    console.log("Жах")
+}
+if (coursesAndDurationArray[1].monthDuration>5) {
+    console.log("Супер")
+} else {
+    console.log("Жах")
+}
+if (coursesAndDurationArray[2].monthDuration>5) {
+    console.log("Супер")
+} else {
+    console.log("Жах")
+}
+if (coursesAndDurationArray[3].monthDuration>5) {
+    console.log("Супер")
+} else {
+    console.log("Жах")
+}
+if (coursesAndDurationArray[4].monthDuration>5) {
+    console.log("Супер")
+} else {
+    console.log("Жах")
+}
+if (coursesAndDurationArray[5].monthDuration>5) {
+    console.log("Супер")
+} else {
+    console.log("Жах")
+}
